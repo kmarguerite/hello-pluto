1413b2729f853ba21e13db3da6c6ac3415cb0fb8
kmarguerite <kmarguerite@mac.com>
2017-08-17 11:46:44 -0700

    added init_balance variable to distinguish between init and current

--- Banking.py
+++ Banking.py
@@ -3,26 +3,26 @@
 class Banking():
     current_balance = 0
 
-    def __init__(self, name, current_balance, x):
+    def __init__(self, name, init_balance, x):
         self.name = name
-        self.current_balance = current_balance
+        self.init_balance = init_balance
         self.x = x
-        print("User, {}, has an account with us with a total of ${}!".format(self.name, self.current_balance))
+        print("User, {}, has an account with us with an initial balance of ${}!".format(self.name, self.init_balance))
 
     #method to deposit
     def deposit(self):
-        raspberry = (self.current_balance + self.x)
-        print("{} have made a deposit of ${}! \n You're new balance is ${}." .format(self.name, self.x, raspberry))
-        return (raspberry)
+        current_balance = (self.init_balance + self.x)
+        print("{} has made a deposit of ${}! \n  The new balance is ${}." .format(self.name, self.x, current_balance))
+        return (current_balance)
 
     #method to withdraw
     def withdraw(self):
-        strawberry = (self.current_balance - self.x)
-        if strawberry < 0:
+        current_balance = (self.init_balance - self.x)
+        if current_balance < 0:
             print("Error! {} will be charged an overdraw fee!".format(self.name))
         else:
-            print("{} have made a withdrawal of ${}! \n You're new balance is ${}.".format(self.name, self.x, strawberry))
-        return (strawberry)
+            print("{} has made a withdrawal of ${}! \n The new balance is ${}.".format(self.name, self.x, current_balance))
+        return (current_balance)
 
 
     #static
@@ -34,13 +34,13 @@ class Banking():
 #error message if overdraws
 
 #create users, include their starting balance
-bank_user1 = Banking(name="Persephone",current_balance=50, x=25)
+bank_user1 = Banking(name="Persephone",init_balance=50, x=25)
 bank_user1.deposit()
 
-bank_user2 = Banking(name="Ariadne",current_balance=500, x=25)
+bank_user2 = Banking(name="Ariadne",init_balance=500, x=25)
 bank_user2.withdraw()
 
-bank_user3 = Banking(name="Hera", current_balance=100, x=225)
+bank_user3 = Banking(name="Hera", init_balance=100, x=225)
 bank_user3.withdraw()
 
 
