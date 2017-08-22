702b06a2079e56adfd5523993ea77ffe457d02e7
kmarguerite <kmarguerite@mac.com>
2017-08-22 11:28:19 -0700

    alter interface to withdraw/deposit by user

--- hw8.py
+++ hw8.py
@@ -9,29 +9,40 @@ from Banking import Banking
 
 #instantiate two users
 
+print("Welcome to Goddess Credit Union(GCU)")
+print("Please provide your name to deposit or withdraw from your account")
+user = input()
+
+if user == "Artemis":
+
+    print("Welcome user Artemis! Your initial balance is $50")
+    selection = int(input("Select 1. for deposit or 2. for withdraw"))
+    raspberry = int(input("How much would you like to deposit/withdraw"))
+
+    bank_user1 = Banking(name="Artemis", init_balance=50, x=raspberry)
+
+    if selection == 1:
+    bank_user1.deposit()
+    if selection ==2:
+    bank_user2.withdraw()
+    print("\n")
 
 
-bank_user1 = Banking(name="Artemis",init_balance=50, x=25)
-bank_user1.deposit()
+
+
+print("Welcome user Persephone. You'r initial amount is $5000")
+selection = int(input("Select 1. for deposit or 2. for withdraw"))
+mango = int(input("How much would you like to deposit/withdraw"))
+bank_user2 = Banking(name="Persephone", init_balance=5000, x=mango)
+
+if selection == 1:
+    bank_user2.deposit()
+if selection == 2:
+    bank_user2.withdraw()
+
 print("\n")
 
-bank_user2 = Banking(name="Persephone",init_balance=500, x=25)
-bank_user2.withdraw()
-print("\n")
-
+print("testing error message")
 bank_user3 = Banking(name="Athena", init_balance=100, x=225)
 bank_user3.withdraw()
 print("\n")
-
-user_name = input("To open an account, please provide your name.")
-user_init = int(input("Please provide a starting balance."))
-user_selection = int(input("Please select 1. for deposit or 2. for withdrawal"))
-user_change = int(input("How much money will you be transferring today?"))
-
-
-bank_user4 = Banking(name=user_name, init_balance=user_init, x=user_change)
-if user_selection == 1:
-    bank_user4.deposit()
-if user_selection == 2:
-    bank_user4.withdraw()
-
