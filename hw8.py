38c2fde53ec261ff4f80b75a82833c43cd55988b
kmarguerite <kmarguerite@mac.com>
2017-08-22 20:06:17 -0700

    automate use of banking module with a new user

--- hw8.py
+++ hw8.py
@@ -1,7 +1,17 @@
-
+#!usr/bin/env Python 3
 
 """
-import Banking script from another file
+This script will execute a banking module for Goddess Online Credit Union
+
+To run this script from the command line type
+python hw8.py
+
+Assumptions:
+
+
+Requires:
+Anaconda Python3
+Banking
 """
 
 import Banking
@@ -9,32 +19,75 @@ from Banking import Banking
 
 #instantiate two users
 
-print("Welcome user Artemis! Your initial balance is $50")
-selection = int(input("Select 1. for deposit or 2. for withdraw"))
-raspberry = int(input("How much would you like to deposit/withdraw"))
+goddesses = ["Artemis", "Persephone"]
 
-bank_user1 = Banking(name="Artemis", init_balance=50, x=raspberry)
-
-if selection == 1:
-bank_user1.deposit()
-if selection ==2:
-bank_user2.withdraw()
-print("\n")
+print("Welcome to Goddess Online Credit Union (Please note, goddesses do not use change. They only use integers)")
+print("The following goddesses have already opened accounts: {}".format(goddesses))
+goddess = input("Please provide your goddess name to deposit or withdraw to your account.")
 
 
-print("Welcome user Persephone. You'r initial amount is $5000")
-selection = int(input("Select 1. for deposit or 2. for withdraw"))
-mango = int(input("How much would you like to deposit/withdraw"))
-bank_user2 = Banking(name="Persephone", init_balance=5000, x=mango)
+a_balance = 3500
+p_balance = 5000
+new_balance = 100
 
-if selection == 1:
-    bank_user2.deposit()
-if selection == 2:
-    bank_user2.withdraw()
+while goddess == "Artemis":
+    print("\n")
+    print("Welcome Artemis! Let me check your balance for you!")
+    print("Your initial balance is ${}".format(a_balance))
+    selection = int(input("Select 1. to deposit or 2. to withdraw 3. to exit"))
+    if selection == 3:
+        exit()
 
-print("\n")
+    raspberry = int(input("How much would you like to deposit/withdraw"))
+    bank_user1 = Banking(name="Artemis", init_balance=a_balance, x=raspberry)
 
-print("testing error message")
-bank_user3 = Banking(name="Athena", init_balance=100, x=225)
-bank_user3.withdraw()
-print("\n")
+    if selection == 1:
+        a_balance = bank_user1.deposit()
+        print("Your current balance is: ${}".format(a_balance))
+    #    print(balance)
+    if selection ==2:
+        a_balance = bank_user1.withdraw()
+        print("Your current balance is: $ {}".format(a_balance))
+    selection = ("Please make another selection. 1. to deposit 2. to withdraw 3. to exit")
+    print("\n")
+
+
+
+while goddess == "Persephone":
+    print("Welcome Persephone! Let me check your balance for you!")
+    print("You're initial balance is ${}".format(p_balance))
+    selection = int(input("Select 1. to deposit or 2. to withdraw 3. to exit"))
+    if selection == 3:
+        exit()
+
+    raspberry = int(input("How much would you like to deposit/withdraw"))
+    bank_user2 = Banking(name="Persephone", init_balance=p_balance, x=raspberry)
+
+    if selection == 1:
+        p_balance = bank_user2.deposit()
+        print("Your current balance is: $ {}".format(p_balance))
+    if selection ==2:
+        print("Your current balance is: ${}".format(p_balance))
+        p_balance = bank_user2.withdraw()
+    selection = int(input("Select 1. to deposit or 2. to withdraw 3. to exit"))
+    print("\n")
+
+
+
+while goddess != "Artemis" or "Persephone":
+    print("Welcome Goddess {}".format(goddess))
+    print("All new accounts start at $100")
+    selection = int(input("Select 1. to deposit or 2. to withdraw 3. to exit"))
+    if selection == 3:
+        exit()
+    raspberry = int(input("How much would you like to deposit/withdraw"))
+    bank_user3 = Banking(name="goddess", init_balance=new_balance, x=raspberry)
+
+    if selection == 1:
+        new_balance = bank_user3.deposit()
+        print("Your current balance is: ${}".format(new_balance))
+
+    #    print(balance)
+    if selection ==2:
+        new_balance = bank_user3.withdraw()
+        print("Your current balance is: ${}".format(new_balance))
